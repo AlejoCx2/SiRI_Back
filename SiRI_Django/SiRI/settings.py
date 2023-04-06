817a7fab5e995f8e0ac2a36dd07a75b4deebbd27
AlejoCx2 <alejandro.caicedo.caicedo@correounivalle.edu.co>
2023-04-05 21:09:48 -0500

    Django vacantes app init

--- SiRI_Django/SiRI/settings.py
+++ SiRI_Django/SiRI/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=ykwpa@f0$)a7s)s#a_#nzh05bty&tir5d*81e4v#$#qa$_r&r'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
