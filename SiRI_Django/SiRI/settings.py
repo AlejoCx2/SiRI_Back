555a1ff31ab46f230e1b6f9ec8964d8c64014261
AlejoCx2 <alejandro.caicedo.caicedo@correounivalle.edu.co>
2023-04-09 20:06:32 -0500

    Creacion de Modelos

--- SiRI_Django/SiRI/settings.py
+++ SiRI_Django/SiRI/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'vacantes',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- SiRI_Django/vacantes/models.py
+++ SiRI_Django/vacantes/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Create your models here.
+class Companies(models.Model):
+    nit = models.CharField(max_length=100, null=False)
+
+    def __str__(self):
+        return self.nit
+
+class Contracts(models.Model):
+    name = models.CharField(max_length=200, null=False)
+
+    def __str__(self):
+        return self.id
+
+class Vacancy(models.Model):
+    idCompany = models.ForeignKey(Companies, on_delete=models.CASCADE, null=False)
+    name = models.CharField(max_length=200, null=False)
+    description = models.TextField(null=False) # max=1500 testiar
+    keyWords = models.CharField(max_length=500, null=False)
+    salary = models.IntegerField(validators=[MinValueValidator(1200000)])
+    idContract = models.ForeignKey(Contracts, on_delete=models.RESTRICT, null=False)
+    experience = models.CharField(max_length=100, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Requirements(models.Model):
+    idVacancy = models.ForeignKey(Vacancy, on_delete=models.CASCADE, null=False)
+    name = models.CharField(max_length=200, null=False)
+    description = models.CharField(max_length=500, null=False)
