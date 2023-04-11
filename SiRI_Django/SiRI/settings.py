1d2090f423f46e33dcde6e3fb31fd2b0a129dde0
AlejoCx2 <alejandro.caicedo.caicedo@correounivalle.edu.co>
2023-04-11 00:04:23 -0500

    Cors sttings and JWT Autentification

--- SiRI_Django/SiRI/settings.py
+++ SiRI_Django/SiRI/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'vacantes',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -55,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 REST_FRAMEWORK = {
@@ -180,3 +182,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ALLOW_ALL_ORIGINS: True
--- SiRI_Django/vacantes/api.py
+++ SiRI_Django/vacantes/api.py
@@ -4,7 +4,7 @@ from rest_framework import viewsets, permissions
 
 class CompaniesViewSet(viewsets.ModelViewSet):
     queryset = Companies.objects.all()
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticated]
     serializer_class = CompaniesSerializers
 
 class ContractsViewSet(viewsets.ModelViewSet):
